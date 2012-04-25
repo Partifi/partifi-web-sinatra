9e5e88b01aaf5c752ddbccddfd411f7e3439147e
Justin Halsall <justin@juice10.com>
2012-04-25 23:57:56 +0200

    add songs and votes tables to migrations

--- db/migrations.rb
+++ db/migrations.rb
@@ -5,3 +5,24 @@ Partifi::DB.create_table :events do
   Time :starts_at
   Time :ends_at
 end
+
+Partifi::DB.create_table :songs do
+  primary_key :id
+  String :name
+  String :artist
+  String :href
+  Interger :event_id
+  Time :created_at
+  Time :updated_at
+  Time :started_at
+  Time :deleted_at
+  add_index [:event_id, :href]
+end
+
+Partifi::DB.create_table :votes do
+  primary_key :id
+  Integer :song_id
+  Integer :user_id
+  enum :status, :elements => ['love', 'hate']
+  add_index [:song_id, :user_id], :unique => true
+end
