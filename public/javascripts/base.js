e8214d5b3c483afeee862c2a41f97b32ada8a496
Kilian <kilian@kilianvalkhof.com>
2012-04-25 21:21:50 +0200

    add base.js

---
+++ public/javascripts/base.js
@@ -0,0 +1,69 @@
+(function ($) {
+  window.fbAsyncInit = function() {
+    $.fb.init();
+  };
+
+  $(function () {
+    // 1 get user to select event OR get it from localstorage
+    // 2 get current playing song and list from matched event
+    // 3 show current playing song, bash button, list, search etc.
+  });
+
+  //facebook functions
+  $.fb = $.fb || {};
+  $.extend($.fb, {
+    init: function () {
+      FB.init({
+        appId: '342859172429389',
+        cookie: true,
+        status: true,
+        xfbml: true
+      });
+
+      this.opts = {
+        name: '',
+        id: '',
+      }
+      //bind login button
+      $(".loginbutton").bind('click', function () {
+        FB.login(function (response) {
+          if (response.session) {
+            $.fb.loggedIn();
+          }
+        }, {scope: 'user_events'});
+        return false;
+      });
+
+      //check current status
+      this.loggedIn();
+    },
+    loggedIn: function () {
+      FB.getLoginStatus(function (response) {
+        if (response.status == "connected") {
+          // logged in and connected user, someone you know
+          $(".loginbutton").hide();
+
+          //show user icon and name
+          FB.api('/me', function(response) {
+            $.fb.opts.id = response.id;
+            $.fb.opts.name = response.name;
+            $(".topbar").append("<span class='user'>" + response.name + "<img src='https://graph.facebook.com/" + response.id + "/picture'></span>");
+          });
+
+          FB.api('/me/events', function(response) {
+            var list = $("<ul></ul>");
+            for (i in response.data) {
+              var name = response.data[i].name,
+                  id   = response.data[i].id;
+              list.append("<li id='"+ id + "'>" + name + "</li>");
+            }
+            list.appendTo("#eventlist");
+          });
+        } else {
+          $(".loginbutton").show();
+        }
+      });
+    },
+  });
+}(jQuery));
+
